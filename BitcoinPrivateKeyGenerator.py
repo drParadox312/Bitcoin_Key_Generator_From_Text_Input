1481f188e412b5339e5abf6e05e44c6821bba0b0
drParadox312 <44335637+drParadox312@users.noreply.github.com>
2023-04-07 01:50:07 +0300

    Update BitcoinPrivateKeyGenerator.py

--- BitcoinPrivateKeyGenerator.py
+++ BitcoinPrivateKeyGenerator.py
@@ -435,7 +435,7 @@ def modify_text(text_input:str):
 
 character_use_count = {"0":0,"1":0,"2":0,"3":0,"4":0,"5":0,"6":0,"7":0,"8":0,"9":0,"a":0,"b":0,"c":0,"d":0,"e":0,"f":0,"g":0,"h":0,"i":0,"j":0,"k":0,"l":0,"m":0,"n":0,"o":0,"p":0,"q":0,"r":0,"s":0,"t":0,"u":0,"v":0,"w":0,"x":0,"y":0,"z":0,"A":0,"B":0,"C":0,"D":0,"E":0,"F":0,"G":0,"H":0,"I":0,"J":0,"K":0,"L":0,"M":0,"N":0,"O":0,"P":0,"Q":0,"R":0,"S":0,"T":0,"U":0,"V":0,"W":0,"X":0,"Y":0,"Z":0,"'":0,",":0,".":0}
 valid_characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',."
-def chech_input_text_is_valid(text_input:str):
+def check_input_text_is_valid(text_input:str):
     characters_length = len(text_input)
     if(len(text_input) < 20):
         return (False, 1)
@@ -588,7 +588,7 @@ if(is_sentence_wrote):
             
         text = modify_text(text)
 
-        (is_text_valid, rejection_code) = chech_input_text_is_valid(text)
+        (is_text_valid, rejection_code) = check_input_text_is_valid(text)
 
         if(is_text_valid):
         
